e6987fc4cdb7ef09ad753adca9dfcc17c016b21e
Surajbarole <surajbarole1@gmail.com>
2023-07-05 20:04:51 +0530

    Added JavaScript logic

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("red-cr").onclick=function(){
+    document.getElementById("red-cr").style.display="none"
+    }
+
+document.getElementById("pink-cr").onclick=function(){
+    document.getElementById("pink-cr").style.display="none"
+    }
+
+document.getElementById("yellow-cr").onclick=function(){
+    document.getElementById("yellow-cr").style.display="none"
+    }
+document.getElementById("white-cr").onclick=function(){
+    document.getElementById("white-cr").style.display="none"
+    }
+document.getElementById("myBtn").onclick=function(){
+        document.getElementById("para").innerHTML="Kaam kar na apna"
+}
